de686edac78d6b75d15b6b36ab645d7c571db2b4
fellipe_leandro <faugusto12@gmail.com>
2017-08-23 14:44:40 -0300

    Continuando

--- troca_regioes/main.cpp
+++ troca_regioes/main.cpp
@@ -3,15 +3,23 @@
 
 using namespace cv;
 using namespace std;
-void dofor(Mat* image,Mat* subimage,int i,int j){
+void dofor(Mat &image,Mat &sub_image,int i_start,int i_end,int j_start,int j_end){
     int i_aux=0;
     int j_aux=0;
+    for(int i=i_start; i<i_end;i++){
+        j_aux=0;
 
+        for(int j=j_start;j<j_end;j++){
+            image.at<uchar>(i,j)=sub_image.at<uchar>(i_aux,j_aux)+0;
+            j_aux++;
+        }
+        i_aux++;
+    }
 
 }
 int main()
 {   Mat image;
-    Mat image1;
+    Mat image1(Size(256,256),CV_8UC1);
     Mat sub_image1,sub_image2,sub_image3,sub_image4;
     image=imread("biel.png",CV_LOAD_IMAGE_GRAYSCALE);
     if(!image.data)
@@ -32,20 +40,23 @@ int main()
 //    namedWindow("janela4",WINDOW_AUTOSIZE);
 //    imshow("janela4", sub_image4);
 //    waitKey();
-    int i_aux=0;
-    int j_aux=0;
-    for(int i=0; i<128;i++){
-        j_aux=0;
+//    int i_aux=0;
+//    int j_aux=0;
+//    for(int i=0; i<128;i++){
+//        j_aux=0;
 
-        for(int j=0;j<128;j++){
-            image.at<uchar>(i,j)=sub_image3.at<uchar>(i_aux,j_aux)+0;
-            j_aux++;
-        }
-        i_aux++;
-    }
-
+//        for(int j=0;j<128;j++){
+//            image.at<uchar>(i,j)=sub_image3.at<uchar>(i_aux,j_aux)+0;
+//            j_aux++;
+//        }
+//        i_aux++;
+//    }
+        dofor(image1,sub_image2,0,128,128,255);  //sub_image2 eh IIIQ e o range eh o primeiro
+        dofor(image1,sub_image3,128,256,0,128);//sub_image3 eh o IQ e o range eh o terceiro
+        dofor(image1,sub_image1,128,256,128,256);//sub_image1 eh o IIQ e o range eh o quarto
+        dofor(image1,sub_image4,0,128,0,128);   //sub_image4 eh o IV4 e o range, o segundo
         namedWindow("janela1",WINDOW_AUTOSIZE);
-        imshow("janela1", image);
+        imshow("janela1", image1);
         waitKey();
 
 
